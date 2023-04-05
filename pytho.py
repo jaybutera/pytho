20efd2c0e6eeededf2ca0306d91cae6d6d8a6df8
Jay Butera <buterajay@protonmail.com>
2023-04-05 22:44:35 +0000

    loop

--- pytho.py
+++ pytho.py
@@ -4,14 +4,16 @@ import os
 
 def main(data_dir):
     files = [os.path.join(data_dir, file) for file in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, file))]
+    print('Building embedding from text corpus...')
 
     docs = Docs()
     for d in files:
         docs.add(d)
 
-    query = input("> ")
-    answer = docs.query(query)
-    print(answer.formatted_answer)
+    while True:
+        query = input("> ")
+        answer = docs.query(query)
+        print(answer.formatted_answer)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='List files in a directory')
